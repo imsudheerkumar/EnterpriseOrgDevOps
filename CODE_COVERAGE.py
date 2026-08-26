f40d082f8f57e8ccdc56e899030b604c85b0e87e
Sudheer <dheer.gsk@gmail.com>
2026-08-27 00:59:34 +0200

    Update default file path in CODE_COVERAGE.py from 'deploy-result.json' to 'sfca_results.json' for improved clarity in coverage calculation.

--- CODE_COVERAGE.py
+++ CODE_COVERAGE.py
@@ -131,5 +131,5 @@ def calculate_coverage(file_path):
 
 
 if __name__ == "__main__":
-    file_path = sys.argv[1] if len(sys.argv) > 1 else 'deploy-result.json'
+    file_path = sys.argv[1] if len(sys.argv) > 1 else 'sfca_results.json'
     calculate_coverage(file_path)
